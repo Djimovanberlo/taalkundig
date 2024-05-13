bf7afe4f9c18a1b6ae2022e0bb4c4affe6a227e3
Djimo van Berlo <djimovanberlo@gmail.com>
2024-05-13 17:40:52 +0200

    update to woff2

--- fragments/card/index.js
+++ fragments/card/index.js
@@ -1,5 +1,5 @@
 import Block from '@/layout/block'
 
-const Card = ({ className, children }) => <Block className={`${className} card`}>{children}</Block>
+const Card = ({ className = '', children }) => <Block className={`${className} card`}>{children}</Block>
 
 export default Card
