d85333a71218322bdb28d57124abde4814d34fe2
Djimovanberlo <djimovanberlo@gmail.com>
2025-02-07 15:52:18 +0100

    rename env vars

--- services/contentful/api/client.js
+++ services/contentful/api/client.js
@@ -1,19 +1,19 @@
-import { GraphQLClient } from 'graphql-request'
+import { GraphQLClient } from "graphql-request";
 
-const CTF_SPACE_ID = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID
-const CTF_ACCESS_TOKEN = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
+const CTF_SPACE_ID = process.env.CONTENTFUL_SPACE_ID;
+const CTF_ACCESS_TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN;
 
-const CONTENTFUL_URL = `https://graphql.contentful.com/content/v1/spaces/${CTF_SPACE_ID}`
+const CONTENTFUL_URL = `https://graphql.contentful.com/content/v1/spaces/${CTF_SPACE_ID}`;
 
 const createClient = () => {
   const client = new GraphQLClient(CONTENTFUL_URL, {
     headers: {
       Authorization: `Bearer ${CTF_ACCESS_TOKEN}`,
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-  })
+  });
 
-  return client
-}
+  return client;
+};
 
-export default createClient
+export default createClient;
